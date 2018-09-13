3bb0f899361329a87497f9140ea6d747a113e317
Софія <sophia@MacBook-Air-Matviisin.local>
2018-09-13 19:44:36 +0300

    isObject change

--- src/isObject/index.js
+++ src/isObject/index.js
@@ -8,7 +8,12 @@
  * isObject(null); // false
  */
 const isObject = data => {
-  /* your logic here...*/
+  if(data instanceof Object){
+      return true;
+  }
+    else{
+        return false;
+    }
 };
 
 export default isObject;
