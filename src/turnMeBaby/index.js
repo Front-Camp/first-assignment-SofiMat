c120b4022480c7f7cfc9283d4f7a0f010967e149
Софія <sophia@MacBook-Air-Matviisin.local>
2018-09-13 20:20:51 +0300

    turnMe change

--- src/turnMeBaby/index.js
+++ src/turnMeBaby/index.js
@@ -8,7 +8,12 @@
  * turnMeBaby('Some text'); // 'txet emoS'
  */
 const turnMeBaby = str => {
-  return str.split("").reverse().join("");
+  //return str.split("").reverse().join("");
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
 };
 
 export default turnMeBaby;
